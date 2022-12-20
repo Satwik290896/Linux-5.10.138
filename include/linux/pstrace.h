211eeb541ce3eb76dd126298cd75023086dfc04d
Vaddi Sai Satwik <vsaisatwik1996@gmail.com>
2022-12-19 22:33:48 -0500

    linux, kernel

---
+++ include/linux/pstrace.h
@@ -0,0 +1,14 @@
+#include <linux/sched.c>
+
+#define PSTRACE_BUF_SIZE 500	/* The maximum size of the ring buffer */
+
+
+/* The data structure used to save the traced process. */
+struct pstrace {
+	char comm[16];	/* name of the process */
+	long state;		/* state of the process */
+	pid_t pid;		/* pid of the process, ie returned by getpid */
+	pid_t tid;		/* tid of the thread, ie returned by gettid */
+};
+
+/* struct pstrace ring_buf[PSTRACE_BUF_SIZE]; */
